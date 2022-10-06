7d61a3c4b1dfd99890766fdd79d5beea431b6af8
Dimpal777 <114867886+Dimpal777@users.noreply.github.com>
2022-10-06 23:33:42 +0530

    Time: 1 ms (43.40%), Space: 46.8 MB (79.61%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,21 +1,33 @@
 class Solution {
     public int[] searchRange(int[] arr, int target) {
-     int []ans = new int[2];
-        ans[0]=-1;
-        ans[1]=-1;
-            
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==target){
-                ans[0]=i;
-                break;
-            }
-              
-           
+     return  findFirstandLastOccurance(arr,target);
     }
-         for(int i=arr.length-1; i>=0; i--){
-            if(arr[i]==target){
-                ans[1]=i;
-                break;
+    static int [] findFirstandLastOccurance(int arr[],int target){
+       int []ans = new int[2];
+       ans[0] =binerySearch(arr,target,true);
+       ans[1] =  binerySearch(arr,target,false);
+        return ans;
+    }
+    static int binerySearch(int []arr, int target,boolean isfirstIndex){
+        int ans = -1;
+     int start = 0;
+        int end  = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;//this is for find middle 
+            if(target>arr[mid]){
+                start = mid+1;
+            }else if(target<arr[mid]){
+                end = mid-1;
+            }else{
+                ans = mid;
+                if(isfirstIndex==true){
+                     end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+                   
+                
+                
             }
         }
         return ans;
