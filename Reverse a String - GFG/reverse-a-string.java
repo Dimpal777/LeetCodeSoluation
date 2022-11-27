464600546f5c221247ca8003faebc86a26ddb26c
Dimpal777 <114867886+Dimpal777@users.noreply.github.com>
2022-11-27 23:15:01 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.java
+++ Reverse a String - GFG/reverse-a-string.java
@@ -1,34 +1,40 @@
 //{ Driver Code Starts
-// Initial template for Java
+//Initial Template for Java
 
 import java.util.*;
 import java.io.*;
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S = read.readLine();
-            Solution ob = new Solution();
+import java.lang.*;
 
-            System.out.println(ob.revStr(S));
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
         }
     }
 }
 // } Driver Code Ends
 
 
-class Solution {
-    static String revStr(String S) {
-        // code here
-        String ans = "";
-        
-        for(int i = S.length()-1; i>=0; i--)
-        {
-            ans += S.charAt(i);
-        }
-        
-        return ans;
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        String s=sb.toString();
+        return s;
     }
 }
